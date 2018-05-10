b37bb8552a95cb58b0e93aa559865fba8bd52f5a
jianwuch <chjianwu@gmail.com>
2018-05-10 18:21:36 +0800

    1.完成页面布局

--- app/src/main/java/com/jianwu/commercialpay/LoginActivity.java
+++ app/src/main/java/com/jianwu/commercialpay/LoginActivity.java
@@ -1,8 +1,14 @@
 package com.jianwu.commercialpay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.gsd.idreamsky.weplay.base.BaseActivity;
 
 /**
@@ -10,6 +16,10 @@ import com.gsd.idreamsky.weplay.base.BaseActivity;
  */
 public class LoginActivity extends BaseActivity {
 
+    @BindView(R.id.account) EditText account;
+    @BindView(R.id.password) EditText password;
+    @BindView(R.id.btn_login) Button btnLogin;
+    @BindView(R.id.btn_sign_in) Button btnSignIn;
 
     @Override
     public int getContentLayoutId() {
@@ -19,9 +29,12 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void handleChildLogic(@Nullable Bundle savedInstanceState) {
 
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN );
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
 
+    @OnClick(R.id.btn_login)
+    public void toMainPage() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
 
